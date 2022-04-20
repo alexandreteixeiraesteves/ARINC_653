9376d29dba5aa5dec880ef06ddc6b03e407d9726
TEIXEIRA ESTEVES Alexandre <alexandre.teixeira-esteves@epita.fr>
2022-04-21 00:56:32 +0200

    TP3 objectif 3 ok

--- TP3/TP3-003-TEIXEIRA_ESTEVES/complex_vec_io.c
+++ TP3/TP3-003-TEIXEIRA_ESTEVES/complex_vec_io.c
@@ -1,20 +1,18 @@
 #include "complex_vec_io.h"
 
-void Complex_io__read_complex_vector_step(Complex_io__read_complex_vector_out*_out){
-	struct Complex__complex complex_vec[3];
+void Complex_vec_io__read_complex_vector_step(Complex_vec_io__read_complex_vector_out*_out){
 	for( size_t i = 0 ; i < 3 ; i++){
 		struct Complex__complex tmp;
 		scanf("%f",&tmp.re) ;
         	scanf("%f",&tmp.im) ;
-		complex_vec[i] = tmp;
+		_out->o[i] = tmp;
 	}
-	_out->o = complex_vec;
 }
 
-void Complex_io__print_complex_vector_step(struct Complex__complex* c,Complex_io__print_complex_vector_out*_out){
+void Complex_vec_io__print_complex_vector_step(struct Complex__complex c[3], Complex_vec_io__print_complex_vector_out*_out){
 	
 	for( size_t i = 0 ; i < 3 ; i++){
 		struct Complex__complex tmp = c[i];
-		printf(" %f + i %f \n", c->re, c->im);
+		printf(" %f + i %f \n", tmp.re, tmp.im);
 	}
 }
--- TP3/TP3-003-TEIXEIRA_ESTEVES/complex_vec_io.h
+++ TP3/TP3-003-TEIXEIRA_ESTEVES/complex_vec_io.h
@@ -6,10 +6,10 @@
 
 #include "complex_c/complex.h"
 
-typedef struct { struct Complex__complex* o; } Complex_io__read_complex_vector_out ;
-typedef struct { } Complex_io__print_complex_vector_out ;
+typedef struct { struct Complex__complex o[3]; } Complex_vec_io__read_complex_vector_out ;
+typedef struct { } Complex_vec_io__print_complex_vector_out ;
 
-void Complex_io__read_complex_vector_step(Complex_io__read_complex_vector_out*_out) ;
-void Complex_io__print_complex_vector_step(struct Complex__complex* c,Complex_io__print_complex_vector_out*_out) ;
+void Complex_vec_io__read_complex_vector_step(Complex_vec_io__read_complex_vector_out*_out) ;
+void Complex_vec_io__print_complex_vector_step(struct Complex__complex c[3],Complex_vec_io__print_complex_vector_out*_out) ;
 
 #endif // COMPLEX_VEC_IO_H
