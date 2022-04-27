ec7ad4a7eeffcee79ff999a4c20e495ff1688bb6
Alexandra Petit <alexandra.petit@epita.fr>
2022-04-27 18:59:36 +0200

    seg

--- TP3/TP3-004-PETIT/compile.sh
+++ TP3/TP3-004-PETIT/compile.sh
@@ -42,14 +42,14 @@ fi
 echo $(pwd)
 cp `heptc -where`/c/pervasives.h build/.
 
-gcc -std=c99 -I$(pwd) -I'build' -c complex_c/complex_types.c complex_c/complex.c -lm -lc -lgcc
-gcc -std=c99 -I$(pwd) -I'build' -c complex_io_types.c complex_io.c -lm -lc -lgcc
-gcc -std=c99 -I$(pwd) -I'build' -c complexes_c/complexes_types.c complexes_c/complexes.c -lm -lc -lgcc
-gcc -std=c99 -I$(pwd) -I'build' -I'complex_c' -c twiddle_init.c twiddle.c -lm -lc -lgcc
-gcc -std=c99 -I$(pwd) -I'build' -c bitrev.c complex_vec_io.c -lm -lc -lgcc
-gcc -std=c99 -I$(pwd) -I'build' -c fft_c/fft.c fft_c/fft_types.c -lm -lc -lgcc
-gcc -std=c99 -I$(pwd) -I'build' -I'bitrev_c' -c fft_test_c/fft_test.c fft_test_c/fft_test_types.c -lm -lc -lgcc
-gcc -std=c99 -I$(pwd) -I'build' -I'complex_c' -I'fft_c' -I'bitrev_c' -c main.c -lm -lc -lgcc
+gcc -std=c99 -g -I$(pwd) -I'build' -c complex_c/complex_types.c complex_c/complex.c -lm -lc -lgcc
+gcc -std=c99 -g -I$(pwd) -I'build' -c complex_io_types.c complex_io.c -lm -lc -lgcc
+gcc -std=c99 -g -I$(pwd) -I'build' -c complexes_c/complexes_types.c complexes_c/complexes.c -lm -lc -lgcc
+gcc -std=c99 -g -I$(pwd) -I'build' -I'complex_c' -c twiddle_init.c twiddle.c -lm -lc -lgcc
+gcc -std=c99 -g -I$(pwd) -I'build' -c bitrev.c complex_vec_io.c -lm -lc -lgcc
+gcc -std=c99 -g -I$(pwd) -I'build' -c fft_c/fft.c fft_c/fft_types.c -lm -lc -lgcc
+gcc -std=c99 -g -I$(pwd) -I'build' -I'bitrev_c' -c fft_test_c/fft_test.c fft_test_c/fft_test_types.c -lm -lc -lgcc
+gcc -std=c99 -g -I$(pwd) -I'build' -I'complex_c' -I'fft_c' -I'bitrev_c' -c main.c -lm -lc -lgcc
 
-gcc -std=c99 -I'build' *.o -o main -lm -lc -lgcc
+gcc -std=c99 -g -I'build' *.o -o main -lm -lc -lgcc
 
--- TP3/TP3-004-PETIT/main.c
+++ TP3/TP3-004-PETIT/main.c
@@ -7,9 +7,13 @@
 #include "fft_test_c/fft_test.h"
 int main() {
 	Fft_test__fft8_out* _out;
+	Complex_io__print_complex_out* _ot;
 	init_twiddle1024(Twiddle__c);
+	/*for(int i= 0; i < 1024; i++) {
+		Complex_io__print_complex_step(Twiddle__c[i], _ot);
+	}
 	printf("main");
-	fflush(stdout);
+	fflush(stdout);*/
 	Fft_test__fft8_step(_out);
 	
 	return 0;
