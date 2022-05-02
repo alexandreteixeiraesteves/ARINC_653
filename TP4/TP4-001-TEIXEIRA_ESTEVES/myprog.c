21674fd3bbac3147424fb2aad349333aea11d806
Alexandra Petit <alexandra.petit@epita.fr>
2022-05-02 16:13:19 +0200

    rectification

--- TP4/TP4-001-TEIXEIRA_ESTEVES/myprog.c
+++ TP4/TP4-001-TEIXEIRA_ESTEVES/myprog.c
@@ -3,10 +3,12 @@
 
 void Myprog__myread_step(int size, Myprog__myread_out*_out){
 	float tmp[size];
-	Sndlib__read_samples_step(&size,tmp);
+	Sndlib__read_samples_out *o;
+	Sndlib__read_samples_step(&size,tmp, o);
 	_out->samples = tmp;
 }
 
 void Myprog__mywrite_step(int size, float *samples , Myprog__mywrite_out*_out){
-	Sndlib__write_samples_step(&size,samples) ; 
+	Sndlib__write_samples_out *o;
+	Sndlib__write_samples_step(&size,samples, o) ; 
 }
