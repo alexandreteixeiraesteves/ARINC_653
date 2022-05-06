5c61ad7acd93fb9fac5cc7efcfd218d1bba61068
TEIXEIRA ESTEVES Alexandre <alexandre.teixeira-esteves@epita.fr>
2022-05-06 17:21:36 +0200

    Avancement TP5

---
+++ TP5/TP5-001-TEIXEIRA_ESTEVES/constants_c/constants.c
@@ -0,0 +1,9 @@
+/* --- Generated the 6/5/2022 at 17:19 --- */
+/* --- heptagon compiler, version 1.05.00 (compiled wed. mar. 30 19:47:23 CET 2022) --- */
+/* --- Command line: /home/teabis/.opam/default/bin/heptc -target c constants.ept --- */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "constants.h"
+
