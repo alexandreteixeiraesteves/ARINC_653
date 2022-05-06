b4208e6b86a1209fc444deaf68ec117ad03ef868
Alexandra Petit <alexandra.petit@epita.fr>
2022-05-06 17:52:50 +0200

    -

--- TP5/TP5-001-TEIXEIRA_ESTEVES/complex_c/complex_types.c
+++
@@ -1,9 +0,0 @@
-/* --- Generated the 6/5/2022 at 17:19 --- */
-/* --- heptagon compiler, version 1.05.00 (compiled wed. mar. 30 19:47:23 CET 2022) --- */
-/* --- Command line: /home/teabis/.opam/default/bin/heptc -target c complex.ept --- */
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "complex_types.h"
-
